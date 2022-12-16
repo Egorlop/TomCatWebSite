87b1b7457b52b1549cc5baf2d5b76c41521d3e04
Egorlop <43143784+Egorlop@users.noreply.github.com>
2022-12-16 03:37:08 +0300

    Delete src/main directory

--- src/main/java/com/example/storehouse/Database.java
+++
@@ -1,8 +0,0 @@
-package com.example.storehouse;
-
-import java.util.ArrayList;
-
-public class Database {
-    public ArrayList<Order> orderList = new ArrayList<Order>();
-    public ArrayList<Person> personList = new ArrayList<Person>();
-}
